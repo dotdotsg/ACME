a74b97bd4b95fae3b661d2e1e69d92769c5f4572
Sumeet Gupta <sumeet.gupta@ria.insure>
2023-09-22 02:05:23 +0530

    exam functins working

--- src/main/java/com/core/acme/DTO/QuestionDTO.java
+++ src/main/java/com/core/acme/DTO/QuestionDTO.java
@@ -1,16 +1,13 @@
 package com.core.acme.DTO;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
